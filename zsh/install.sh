caf39f9b5a20614dc09df7432a949820ff11e8db
Werner Stein <wstein@users.noreply.github.com>
2020-12-15 22:47:45 +0100

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,19 +4,19 @@
 echo configuring zsh...
 
 # powerline10k config
-test -d \$HOME/.config/p10k || mkdir --parents \$HOME/.config/p10k
-test -d \$HOME/bin || mkdir --parents \$HOME/bin
-test -d \$HOME/.local/bin || mkdir --parents \$HOME/.local/bin
+test -d $HOME/.config/p10k || mkdir --parents $HOME/.config/p10k
+test -d $HOME/bin || mkdir --parents $HOME/bin
+test -d $HOME/.local/bin || mkdir --parents $HOME/.local/bin
 
-curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/lean-ansi.zsh >\$HOME/.config/p10k/lean-ansi.zsh
-curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/lean-unicode.zsh >\$HOME/.config/p10k/lean-unicode.zsh
-curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/rainbow-ansi.zsh >\$HOME/.config/p10k/rainbow-ansi.zsh
-curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/rainbow-unicode.zsh >\$HOME/.config/p10k/rainbow-unicode.zsh
-curl -fsSL git.io/antigen >"\$HOME/.local/bin/antigen.zsh"
+curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/lean-ansi.zsh >$HOME/.config/p10k/lean-ansi.zsh
+curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/lean-unicode.zsh >$HOME/.config/p10k/lean-unicode.zsh
+curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/rainbow-ansi.zsh >$HOME/.config/p10k/rainbow-ansi.zsh
+curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/rainbow-unicode.zsh >$HOME/.config/p10k/rainbow-unicode.zsh
+curl -fsSL git.io/antigen >"$HOME/.local/bin/antigen.zsh"
 
-if ! [ -d \$HOME/.config/kitty ]; then
-	test -d \$HOME/.config/kitty || mkdir --parents \$HOME/.config/kitty
-	cat <<-EOF >\$HOME/.config/kitty/kitty.conf
+if ! [ -d $HOME/.config/kitty ]; then
+	test -d $HOME/.config/kitty || mkdir --parents $HOME/.config/kitty
+	cat <<-EOF >$HOME/.config/kitty/kitty.conf
 		font_family     FiraCodeNerdFontCompleteM-Medium
 		font_features   FiraCodeNerdFontCompleteM-Medium +ss04 +ss07 +zero
 	EOF
@@ -34,6 +34,8 @@ cat >$HOME/.zshrc <<-EOF
 	# Bundles from the default repo (robbyrussell's oh-my-zsh).
 	#antigen bundle github <- does not work with get-extras
 	antigen bundle git
+	antigen bundle tj/git-extras
+
 	antigen bundle git-extras
 	antigen bundle fossil
 	antigen bundle docker
@@ -47,9 +49,9 @@ cat >$HOME/.zshrc <<-EOF
 
 	# other bundles.
 	antigen bundle denysdovhan/gitio-zsh
+	antigen bundle nvie/git-toolbelt
 	antigen bundle wstein/git-toolbelt-zsh
-	antigen bundle tj/git-extras
-
+	
 	# Load the theme.
 	antigen theme romkatv/powerlevel10k
 
