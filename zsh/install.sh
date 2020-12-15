9ae9be2643efa725c9856086bbe289ce8fddc864
Werner Stein <wstein@users.noreply.github.com>
2020-12-15 22:35:20 +0100

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -15,56 +15,56 @@ curl -fsSL https://raw.github.com/wstein/setup/develop/config/p10k/rainbow-unico
 curl -fsSL git.io/antigen >"\$HOME/.local/bin/antigen.zsh"
 
 if ! [ -d \$HOME/.config/kitty ]; then
-  test -d \$HOME/.config/kitty || mkdir --parents \$HOME/.config/kitty
-  cat <<-EOF >\$HOME/.config/kitty/kitty.conf
-    font_family     FiraCodeNerdFontCompleteM-Medium
-    font_features   FiraCodeNerdFontCompleteM-Medium +ss04 +ss07 +zero
-  EOF
+	test -d \$HOME/.config/kitty || mkdir --parents \$HOME/.config/kitty
+	cat <<-EOF >\$HOME/.config/kitty/kitty.conf
+		font_family     FiraCodeNerdFontCompleteM-Medium
+		font_features   FiraCodeNerdFontCompleteM-Medium +ss04 +ss07 +zero
+	EOF
 fi
 
 cat >$HOME/.zshrc <<-EOF
-  source \$HOME/.local/bin/antigen.zsh
+	source \$HOME/.local/bin/antigen.zsh
 
-  # extend path
-  export PATH="\$HOME/bin:\$HOME/.local/bin:\$PATH"
+	# extend path
+	export PATH="\$HOME/bin:\$HOME/.local/bin:\$PATH"
 
-  # Load the oh-my-zsh's library.
-  antigen use oh-my-zsh
+	# Load the oh-my-zsh's library.
+	antigen use oh-my-zsh
 
-  # Bundles from the default repo (robbyrussell's oh-my-zsh).
-  #antigen bundle github <- does not work with get-extras
-  antigen bundle git
-  antigen bundle git-extras
-  antigen bundle fossil
-  antigen bundle docker
-  antigen bundle docker-compose
-  antigen bundle tmux
-  antigen bundle command-not-found
+	# Bundles from the default repo (robbyrussell's oh-my-zsh).
+	#antigen bundle github <- does not work with get-extras
+	antigen bundle git
+	antigen bundle git-extras
+	antigen bundle fossil
+	antigen bundle docker
+	antigen bundle docker-compose
+	antigen bundle tmux
+	antigen bundle command-not-found
 
-  # Load the zsh-users bundles.
-  antigen bundle zsh-users/zsh-autosuggestions
-  antigen bundle zsh-users/zsh-syntax-highlighting
+	# Load the zsh-users bundles.
+	antigen bundle zsh-users/zsh-autosuggestions
+	antigen bundle zsh-users/zsh-syntax-highlighting
 
-  # other bundles.
-  antigen bundle denysdovhan/gitio-zsh
-  antigen bundle wstein/git-toolbelt-zsh
-  antigen bundle tj/git-extras
+	# other bundles.
+	antigen bundle denysdovhan/gitio-zsh
+	antigen bundle wstein/git-toolbelt-zsh
+	antigen bundle tj/git-extras
 
-  # Load the theme.
-  antigen theme romkatv/powerlevel10k
+	# Load the theme.
+	antigen theme romkatv/powerlevel10k
 
-  # Tell Antigen that you're done.
-  antigen apply
+	# Tell Antigen that you're done.
+	antigen apply
 
-  # aliases
-  #alias ls=exa
-  #alias open=xdg-open
+	# aliases
+	#alias ls=exa
+	#alias open=xdg-open
 
-  # To customize prompt, run \$(p10k configure).
-  if [ -n "\$SSH_TTY" ] || [ "\$TERM" = "linux" ]; then
-    P10K_CONFIG="\$HOME/.config/p10k/lean-ansi.zsh"
-  else
-    P10K_CONFIG="\$HOME/.config/p10k/rainbow-unicode.zsh"
-  fi
-  test -f \$P10K_CONFIG && source \$P10K_CONFIG
+	# To customize prompt, run \$(p10k configure).
+	if [ -n "\$SSH_TTY" ] || [ "\$TERM" = "linux" ]; then
+		P10K_CONFIG="\$HOME/.config/p10k/lean-ansi.zsh"
+	else
+		P10K_CONFIG="\$HOME/.config/p10k/rainbow-unicode.zsh"
+	fi
+	test -f \$P10K_CONFIG && source \$P10K_CONFIG
 EOF
